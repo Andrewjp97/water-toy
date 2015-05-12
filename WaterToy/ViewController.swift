b659e4a0b2bb2fe7d2dd870fe3b311d1e408643a
Andrew Paterson <andrewpaterson@Andrews-MacBook-Air.local>
2015-05-12 19:27:31 -0400

    Initial Commit

---
+++ WaterToy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaterToy
+//
+//  Created by Andrew Paterson on 5/12/15.
+//  Copyright (c) 2015 Andrew Paterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
